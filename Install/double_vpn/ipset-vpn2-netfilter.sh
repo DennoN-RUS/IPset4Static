fc050fbdaa05eefb5d789201abedf57189555799
DennoN-RUS <dennon.ru@gmail.com>
2024-08-06 13:20:42 +0300

    fix netfilter

--- Install/double_vpn/ipset-vpn2-netfilter.sh
+++ Install/double_vpn/ipset-vpn2-netfilter.sh
@@ -8,6 +8,6 @@
 [ -z "$(ipset --quiet list ipset_vpn2)" ] && exit
 
 if [ -z "$(iptables-save | grep ipset_vpn2)" ]; then
-     iptables -w -t mangle -A PREROUTING ! -s $VPN2_SUBNET -m conntrack --ctstate NEW -m set --match-set ipset_vpn1 dst -j CONNMARK --set-mark 1012
-     iptables -w -t mangle -A PREROUTING ! -s $VPN2_SUBNET -m set --match-set ipset_vpn1 dst -j CONNMARK --restore-mark
+     iptables -w -t mangle -A PREROUTING ! -s $VPN2_SUBNET -m conntrack --ctstate NEW -m set --match-set ipset_vpn2 dst -j CONNMARK --set-mark 1012
+     iptables -w -t mangle -A PREROUTING ! -s $VPN2_SUBNET -m set --match-set ipset_vpn2 dst -j CONNMARK --restore-mark
 fi
